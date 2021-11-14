949352dfaae1d0af93e57a2e64824e100a512832
Omegon226 <ivan_m_smirnov@mail.ru>
2021-11-14 21:56:14 +0300

    V4

---
+++ BLL/Services/FindeCruise.cs
@@ -0,0 +1,43 @@
+﻿using BLL.Models;
+using DAL.EFClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class FindeCruise
+    {
+        public class StoredProcedureResult
+        {
+            public int ID { get; set; }
+            public int DayOfTheWeekCruiseID { get; set; }
+            public int RouteIDOfTheCruise { get; set; }
+            public int DriverIDOfTheCruise { get; set; }
+            public int TransportIDOfTheCruise { get; set; }
+            public TimeSpan? StartTime { get; set; }
+        }
+
+        public static List<StoredProcedureResult> StoredProcedureExecute(int Time)
+        {
+            System.Data.SqlClient.SqlParameter param1 = new System.Data.SqlClient.SqlParameter("@TravelTimeRoad", Time);
+            AvtovokzalDBContext DBContext = new AvtovokzalDBContext();
+            var result = DBContext.Database.SqlQuery<StoredProcedureResult>("SP_FindeCruiseUsingTravelTimeRoad @TravelTimeRoad", new object[] { param1 }).ToList();
+
+            var data = result.GroupBy(i => new { i.ID, i.DayOfTheWeekCruiseID, i.RouteIDOfTheCruise, i.DriverIDOfTheCruise, i.TransportIDOfTheCruise, i.StartTime })
+                .Select(i => new StoredProcedureResult
+                {
+                    ID = i.Key.ID,
+                    DayOfTheWeekCruiseID = i.Key.DayOfTheWeekCruiseID,
+                    RouteIDOfTheCruise = i.Key.RouteIDOfTheCruise,
+                    DriverIDOfTheCruise = i.Key.DriverIDOfTheCruise,
+                    TransportIDOfTheCruise = i.Key.TransportIDOfTheCruise,
+                    StartTime = i.Key.StartTime
+                }).ToList();
+            return (data);
+
+        }
+    }
+}
---
+++ BLL/Services/FindeRoute.cs
@@ -0,0 +1,65 @@
+﻿using DAL.EFClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+
+    public class FindeRoute
+    {
+        public class StoredProcedureResult1
+        {
+            public int ID { get; set; }
+            public int StopRouteID { get; set; }
+            public int? TravelTimeInHours { get; set; }
+            public bool? Hidden { get; set; }
+        }
+
+        public class StoredProcedureResult2
+        {
+            public int StopRouteID { get; set; }
+            public int? TravelTimeInHours { get; set; }
+            public bool? Hidden { get; set; }
+        }
+
+        public static List<StoredProcedureResult1> StoredProcedureExecute1(int StartPoint, int EndPoint)
+        {
+            System.Data.SqlClient.SqlParameter param1 = new System.Data.SqlClient.SqlParameter("@FirstLocalityName", StartPoint);
+            System.Data.SqlClient.SqlParameter param2 = new System.Data.SqlClient.SqlParameter("@LastLocalityName", EndPoint);
+            AvtovokzalDBContext DBContext = new AvtovokzalDBContext();
+            var result = DBContext.Database.SqlQuery<StoredProcedureResult1>("SP_FindeRoutesAndStoppingSequencesIDByTwoLocations @LastLocalityName,@FirstLocalityName", new object[] { param2, param1 }).ToList();
+
+            var data = result.GroupBy(i => new { i.ID, i.StopRouteID, i.TravelTimeInHours, i.Hidden })
+                .Select(i => new StoredProcedureResult1
+                {
+                    ID = i.Key.ID,
+                    StopRouteID = i.Key.StopRouteID,
+                    TravelTimeInHours = i.Key.TravelTimeInHours,
+                    Hidden = i.Key.Hidden
+                }).ToList();
+
+            return (data);
+
+        }
+
+        public static List<StoredProcedureResult2> StoredProcedureExecute2(int EndPoint)
+        {
+            System.Data.SqlClient.SqlParameter param1 = new System.Data.SqlClient.SqlParameter("@LastStopID", EndPoint);
+            AvtovokzalDBContext DBContext = new AvtovokzalDBContext();
+            var result = DBContext.Database.SqlQuery<StoredProcedureResult2>("SP_FindeRouteByFinalStopID @LastStopID", new object[] { param1 }).ToList();
+
+            var data = result.GroupBy(i => new { i.StopRouteID, i.TravelTimeInHours, i.Hidden })
+                .Select(i => new StoredProcedureResult2
+                {
+                    StopRouteID = i.Key.StopRouteID,
+                    TravelTimeInHours = i.Key.TravelTimeInHours,
+                    Hidden = i.Key.Hidden
+                }).ToList();
+            return (data);
+
+        }
+    }
+}
